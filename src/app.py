e19d722976a00527bdf10328deeebcb80327a13c
Athroniaeth <pierre.chaumont@hotmail.fr>
2024-07-02 05:00:12 +0200

    Init application

---
+++ src/app.py
@@ -0,0 +1,31 @@
+import pygame
+
+
+def app(fps=30):
+    pygame.init()
+
+    screen_info = pygame.display.Info()  # noqa: F841
+    screen_size = (1280, 720)
+
+    screen = pygame.display.set_mode(screen_size)
+    clock = pygame.time.Clock()
+
+    while True:
+        events = _get_events()
+
+        screen.fill((225, 225, 225))
+
+        pygame.display.update()
+        clock.tick(fps)
+
+
+def _get_events():
+    events = pygame.event.get()
+    generator = (event for event in events if event.type == pygame.QUIT)
+    user_ask_quit = next(generator, False)
+
+    if user_ask_quit:
+        pygame.quit()
+        raise SystemExit(0)
+
+    return events
