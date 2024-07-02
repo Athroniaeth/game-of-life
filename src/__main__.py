718d894be0b5a6a1b27aedd2962d6da0161f4eac
Athroniaeth <pierre.chaumont@hotmail.fr>
2024-07-02 03:49:27 +0200

    Init source file

---
+++ src/__main__.py
@@ -0,0 +1,34 @@
+import logging
+import sys
+from pathlib import Path
+
+path = Path(__file__).parents[1].absolute()
+sys.path.append(f'{path}')
+
+from src.app import app  # noqa: E402
+
+
+def main():
+    return_code = 0
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    try:
+        app()
+    except KeyboardInterrupt as exception:
+        logging.info(f"Exiting the program: '{exception}'")
+
+    except SystemExit as exception:
+        logging.info(f"Exiting the program: '{exception}'")
+        return_code = exception.code
+
+    except Exception as exception:
+        logging.error(f"An error occurred: '{exception}'")
+        return_code = 1
+
+    finally:
+        sys.exit(return_code)
+
+
+if __name__ == '__main__':
+    main()
