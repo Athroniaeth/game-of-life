87de7565ad61443b2ea3b392a9cd614dad0f3ce9
Athroniaeth <pierre.chaumont@hotmail.fr>
2024-07-06 04:02:14 +0200

    Update, Add Typer CLI system, update dependencies

--- src/app.py
+++ src/app.py
@@ -4,44 +4,49 @@ from src.grid import GridModel, GridView, GridController
 from src.mouse import MouseInfo
 
 
-def app(fps=30):
-    pygame.init()
+class Game:
+    def __init__(self, fps=30):
+        pygame.init()
+        self.fps = fps
 
-    screen_info = pygame.display.Info()  # noqa: F841
-    screen_size = (1280, 720 + 50)
+        screen_info = pygame.display.Info()  # noqa: F841
+        self.screen_size = (1280, 720 + 50)
 
-    screen = pygame.display.set_mode(screen_size)
-    clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode(self.screen_size)
+        self.clock = pygame.time.Clock()
 
-    # Reserve specific area for the grid
-    screen_grid = screen.subsurface((0, 0, screen_size[0], screen_size[1] - 50))
+        # Reserve specific area for the grid
+        self.screen_grid = self.screen.subsurface((0, 0, self.screen_size[0], self.screen_size[1] - 50))
 
-    grid_model = GridModel(shape=(16, 9))
-    grid_view = GridView(screen=screen_grid)
-    grid_controller = GridController(grid_model, grid_view)
+        self.grid_model = GridModel(shape=(16, 9))
+        self.grid_view = GridView(screen=self.screen_grid)
+        self.grid_controller = GridController(self.grid_model, self.grid_view)
 
-    mouse_info = MouseInfo()
+        self.mouse_info = MouseInfo()
+        self.font = pygame.font.Font(None, 32)
 
-    while True:
-        events = _get_events()
-        mouse_info.update(events)
+    def run(self):
+        while True:
+            events = self._get_events()
+            self.mouse_info.update(events)
 
-        screen.fill((225, 225, 225))
+            self.screen.fill((225, 225, 225))
 
-        grid_controller.handle_event(mouse_info)
-        grid_controller.draw()
+            self.grid_controller.handle_event(self.mouse_info)
+            self.grid_controller.draw()
 
-        pygame.display.update()
-        clock.tick(fps)
+            pygame.display.update()
+            self.clock.tick(self.fps)
 
+    @staticmethod
+    def _get_events():
+        events = pygame.event.get()
+        generator = (event for event in events if event.type == pygame.QUIT)
+        user_ask_quit = next(generator, False)
 
-def _get_events():
-    events = pygame.event.get()
-    generator = (event for event in events if event.type == pygame.QUIT)
-    user_ask_quit = next(generator, False)
+        if user_ask_quit:
+            pygame.quit()
+            raise SystemExit(0)
 
-    if user_ask_quit:
-        pygame.quit()
-        raise SystemExit(0)
+        return events
 
-    return events
