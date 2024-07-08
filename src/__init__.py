c9800ec14edfe69a715395ad0cb51a963c962352
Athroniaeth <pierre.chaumont@hotmail.fr>
2024-07-08 03:44:10 +0200

    Add alpha possibility color & minor correction

--- src/__init__.py
+++ src/__init__.py
@@ -2,3 +2,5 @@ from pathlib import Path
 
 PROJECT_PATH = Path(__file__).parents[1].absolute()
 SOURCE_PATH = PROJECT_PATH / 'src'
+
+STATIC_PATH = PROJECT_PATH / 'static'
--- src/app.py
+++ src/app.py
@@ -20,12 +20,12 @@ class Game:
         self.screen = pygame.display.set_mode(self.screen_size)
         self.clock = pygame.time.Clock()
 
-        self.grid_model = GridModel(shape=(16, 9))
+        self.grid_model = GridModel(shape=(32, 18))
         self.grid_view = GridView(screen=self.screen)
         self.grid_controller = GridController(self.grid_model, self.grid_view)
 
         self.mouse_info = MouseInfo()
-        self.input_text = InputTextCLI(self.cli, 0, 670, 1280, 50, active=False, font_size=25)
+        self.input_text = InputTextCLI(self.cli, 0, 670, 1280, 50, active=False, font_size=27)
 
     def run(self):
         while True:
