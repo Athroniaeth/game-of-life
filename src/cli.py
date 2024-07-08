3ca5067a0ed379d64d1b88fc99258c6a236ace31
Athroniaeth <pierre.chaumont@hotmail.fr>
2024-07-08 10:36:57 +0200

    Update, add history to console

--- src/cli.py
+++ src/cli.py
@@ -1,17 +1,28 @@
 import typer
+from typer.testing import CliRunner
 
-from src import app as module_app
-
-
-cli = typer.Typer()
+cli = typer.Typer(
+    name="",
+    add_completion=False,
+    no_args_is_help=False,
+    help="This is a simple CLI app.",
+    add_help_option=True,
+    context_settings={"help_option_names": ["--help"]},
+)
 
 
 @cli.command()
 def hello(name: str):
-    module_app.app.grid_model.grid[0][0] = 1
-    print(f"Hello {name}")
+    typer.echo(f"hello {name}")
 
 
-@cli.command()
-def _(command: str):
-    print(f"Command: {command}")
+@cli.command(name="help", help="Display help message, list of commands.")
+def _help():
+    runner = CliRunner()
+    result = runner.invoke(cli, ["--help"])
+    typer.echo(result.stdout)
+    print(result.stdout)
+
+
+if __name__ == "__main__":
+    cli()
